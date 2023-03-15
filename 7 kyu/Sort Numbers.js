b23eabac50f65d44da2ab9ecfe8cab8255a30d01
Dmitry <skokov.dmitry97@gmail.com>
2023-03-15 15:42:35 +0500

    .

--- 7 kyu/Sort Numbers.js
+++ 7 kyu/Sort Numbers.js
@@ -1,6 +1,3 @@
-// https://www.codewars.com/kata/5174a4c0f2769dd8b1000003 
+// https://www.codewars.com/kata/5174a4c0f2769dd8b1000003
 
-function solution(nums){
-return !isFinite(nums) ? nums.sort((a,b) => a - b) : []
-}
-console.log(solution([]))
+const solution = nums => (!isFinite(nums) ? nums.sort((a, b) => a - b) : [])
