24db24c66098f4555029156a2de3f55460dad6f2
Dmitry Skokov <skokovdmitry97@gmail.com>
2023-03-24 15:56:49 +0500

    .

--- 7 kyu/Remove the minimum.js
+++ 7 kyu/Remove the minimum.js
@@ -1,9 +1,8 @@
 // https://www.codewars.com/kata/563cf89eb4747c5fb100001b
 
 function removeSmallest(numbers) {
-const copyNumbers = numbers.slice()
-const findMin = Math.min(...copyNumbers)
-copyNumbers.splice(copyNumbers.indexOf(findMin), 1)
-return copyNumbers
-  }
-  console.log(removeSmallest([5, 3, 2, 1, 4]))
+  const copyNumbers = numbers.slice()
+  const findMin = Math.min(...copyNumbers)
+  copyNumbers.splice(copyNumbers.indexOf(findMin), 1)
+  return copyNumbers
+}
